8754264459d4c77433118f6cfd23348bcd413d59
jamilkhalaf <jamil04k@byu.edu>
2025-02-06 08:52:07 -0700

    changes

--- components/game.jsx
+++ components/game.jsx
@@ -8,18 +8,15 @@ const Game = () => {
         initializeGame();
       }, []);
     return (
-        <main class="game">
-        <h2>Memory Game</h2>
-        <p>The game will appear below. Get ready to have some fun!</p>
-    
-        <div class="myGame">
+        <main className="game">
+        <h2>Memory Game</h2>    
             <h3>Score: <span id="result"></span></h3>
             <div id="timeElasped">Time: <span id="time"></span></div>
             <button id="restartButton">Restart Game</button>
             <div className="grid-container">
                     <div className="grid"></div>
             </div>
-        </div>
+        
     
         <div class="placeholder-container">
             <p id="third-party-placeholder">(Placeholder for service call to fetch images when a player wins)</p>
