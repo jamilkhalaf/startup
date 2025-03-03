c569a2d1a225ef8ecf5664948f7f04bc0cdee24f
jamilkhalaf <jamil04k@byu.edu>
2025-03-02 17:37:16 -0700

    changes

--- service/index.js
+++ service/index.js
@@ -119,7 +119,7 @@ function clearAuthCookie(res, user) {
   res.clearCookie('token');
 }
 
-const port = 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
